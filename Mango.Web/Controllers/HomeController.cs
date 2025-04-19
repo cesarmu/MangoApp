f535bddd5d7799370621fed3a7dac7907174ef10
Cesar <cesarmu@yahoo.com>
2025-04-19 14:41:04 -0600

    cambios en el controller web

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -58,6 +58,7 @@ namespace Mango.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
